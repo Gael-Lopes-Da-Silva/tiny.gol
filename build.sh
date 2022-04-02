bdd4708015c1b7c881a542fd97b61397def2a287
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-04-03 01:49:53 +0200

    ⚙update build method

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-GOOS=windows go -ldflags -H=windowsgui build .
+# todo: build for all platforms https://github.com/gen2brain/raylib-go
+GOOS=windows GOARCH=amd64  go build -ldflags="-H=windowsgui" .
