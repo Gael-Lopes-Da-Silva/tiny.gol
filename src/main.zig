e223223a0c7dbe81bf1cd7d4ffeba56706d56f4d
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-02-10 01:06:19 +0100

    added turn count

--- src/main.zig
+++ src/main.zig
@@ -4,86 +4,108 @@
 
 const std = @import("std");
 
-const WIDTH: i32 = 20;
-const HEIGHT: i32 = 20;
+const WIDTH: u32 = 20;
+const HEIGHT: u32 = 20;
 
 const Cell = struct {
     state: enum { Dead, Alive } = .Dead,
 };
 
-fn countNeighbors(frame: [HEIGHT][WIDTH]Cell, rows: usize, cols: usize) i32 {
-    var count: i32 = 0;
+const CellGrid = struct {
+    const This = @This();
 
-    for (rows..rows + 3) |x| {
-        for (cols..cols + 3) |y| {
-            if (@as(i32, @intCast(x)) - 1 == rows and @as(i32, @intCast(y)) - 1 == cols) continue;
+    grid: [HEIGHT][WIDTH]Cell = undefined,
 
-            const row: usize = @intCast(@mod(@as(i32, @intCast(x)) - 1, HEIGHT));
-            const col: usize = @intCast(@mod(@as(i32, @intCast(y)) - 1, WIDTH));
+    fn setupGrid(this: This) [HEIGHT][WIDTH]Cell {
+        var grid = this.grid;
 
-            if (frame[row][col].state == .Alive) count += 1;
-        }
-    }
-
-    return count;
-}
-
-fn nextFrame(frame: [HEIGHT][WIDTH]Cell) [HEIGHT][WIDTH]Cell {
-    var next: [HEIGHT][WIDTH]Cell = frame;
-
-    for (frame, 0..) |row, x| {
-        for (row, 0..) |cell, y| {
-            const neighbors: i32 = countNeighbors(frame, x, y);
-
-            // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-            // Any live cell with two or three live neighbours lives on to the next generation.
-            // Any live cell with more than three live neighbours dies, as if by overpopulation.
-            // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
-
-            if (cell.state == .Alive and (neighbors < 2 or neighbors > 3)) {
-                next[x][y].state = .Dead;
-            } else if (cell.state == .Dead and neighbors == 3) {
-                next[x][y].state = .Alive;
+        var x: u32 = 0;
+        while (HEIGHT < x) : (x += 1) {
+            var y: u32 = 0;
+            while (WIDTH < y) : (y += 1) {
+                grid[x][y].state = .Dead;
             }
         }
+
+        return grid;
     }
 
-    return next;
-}
+    fn countNeighbors(this: This, rows: usize, cols: usize) u32 {
+        var count: u32 = 0;
 
-fn drawFrame(frame: [HEIGHT][WIDTH]Cell) !void {
-    const stdout = std.io.getStdOut().writer();
+        for (rows..rows + 3) |x| {
+            for (cols..cols + 3) |y| {
+                if (@as(u32, @intCast(x)) - 1 == rows and @as(u32, @intCast(y)) - 1 == cols) continue;
 
-    for (frame) |row| {
-        for (row) |cell| {
-            try stdout.writeAll(switch (cell.state) {
-                .Alive => "\u{1B}[40m  \u{1B}[0m",
-                .Dead => "\u{1B}[47m  \u{1B}[0m",
-            });
+                const row: usize = @intCast(@mod(@as(u32, @intCast(x)) - 1, HEIGHT));
+                const col: usize = @intCast(@mod(@as(u32, @intCast(y)) - 1, WIDTH));
+
+                if (this.grid[row][col].state == .Alive) count += 1;
+            }
         }
-        try stdout.writeAll("\n");
+
+        return count;
     }
-}
+
+    fn nextGrid(this: This) [HEIGHT][WIDTH]Cell {
+        var grid = this.grid;
+
+        var x: u32 = 0;
+        while (HEIGHT < x) : (x += 1) {
+            var y: u32 = 0;
+            while (WIDTH < y) : (y += 1) {
+                const neighbors: u32 = this.countNeighbors(x, y);
+
+                // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                // Any live cell with two or three live neighbours lives on to the next generation.
+                // Any live cell with more than three live neighbours dies, as if by overpopulation.
+                // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+
+                if (this.grid[x][y].state == .Alive and (neighbors < 2 or neighbors > 3)) {
+                    grid[x][y].state = .Dead;
+                } else if (this.grid[x][y].state == .Dead and neighbors == 3) {
+                    grid[x][y].state = .Alive;
+                }
+            }
+        }
+
+        return grid;
+    }
+
+    fn drawGrid(this: This, count: u64) !void {
+        const stdout = std.io.getStdOut().writer();
+
+        for (this.grid) |row| {
+            for (row) |cell| {
+                try stdout.writeAll(switch (cell.state) {
+                    .Alive => "\u{1B}[40m  \u{1B}[0m",
+                    .Dead => "\u{1B}[47m  \u{1B}[0m",
+                });
+            }
+            try stdout.writeAll("\n");
+        }
+
+        try stdout.print("{}\n", .{count});
+    }
+};
 
 fn resetCursor() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("\u{1B}[{}A", .{HEIGHT});
+    try stdout.print("\u{1B}[{}A", .{HEIGHT + 1});
     try stdout.print("\u{1B}[{}D", .{WIDTH});
 }
 
 pub fn main() !void {
-    var frame: [HEIGHT][WIDTH]Cell = undefined;
+    var cellGrid: CellGrid = CellGrid{};
+    var count: u64 = 0;
 
-    frame[0][1].state = .Alive;
-    frame[1][2].state = .Alive;
-    frame[2][2].state = .Alive;
-    frame[2][1].state = .Alive;
-    frame[2][0].state = .Alive;
-
+    cellGrid.grid = cellGrid.setupGrid();
     while (true) {
-        try drawFrame(frame);
-        frame = nextFrame(frame);
+        try cellGrid.drawGrid(count);
+        cellGrid.grid = cellGrid.nextGrid();
         try resetCursor();
         std.time.sleep(std.time.ns_per_s * 0.1);
+
+        count += 1;
     }
 }
